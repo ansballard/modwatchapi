ea8e2f80320a4ce8f96ac3687d4f959247182813
Aaron Ballard <ans.ballard@gmail.com>
2015-03-15 11:51:30 -0500

    needed the config file

---
+++ config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	'url':'mongodb://aaron:themotherofallpasswords@ds027708.mongolab.com:27708/skyrim',
+	'devurl':'mongodb://aarondev:themotherofallpasswords@ds049570.mongolab.com:49570/skyrimdev3',
+	'local':'localhost:27017/skyrim'
+};
